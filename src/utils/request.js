9b97f864571b490685c124be206bc2bab9da0da6
xudan <875126243@qq.com>
2023-02-02 10:59:43 +0800

    fix add aios interceptor

---
+++ src/utils/request.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import store from "../store";
+import router from "../router";
+const baseURL = "http://apipc-xiaotuxian-front.itheima.net";
+const instance = axios.create({
+    baseURL,
+    timeout: 5000
+})
+// 请求拦截器
+instance.interceptors.request.use(
+    (config) => {
+        const { profile } = store.state.user
+        if(profile.token) {
+            config.headers.Authorization = `Bearer ${profile.token}`
+        }
+        return config
+    },
+    (err) => {
+        return Promise.reject(err)
+    },
+)
+
+instance.interceptors.response.use(
+    (res) => {
+       return res.data
+    },
+    (err) => {
+        if(err.response && err.response.status === 401) {
+           store.commit("user/setUser", {});
+           const fullPath = encodeURIComponent(router.currentRoute.value.fullPath);
+           router.push("/login/redirecturl" + fullPath)
+        }
+        return Promise.reject(err)
+    },
+)
+
+export default(url, method, submitData)=>{
+    return instance({
+        url,
+        method,
+        [params.toLowerCase() === "get" ? "params" : "data"]:submitData,
+    })
+}
